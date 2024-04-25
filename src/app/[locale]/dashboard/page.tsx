4804952831c022290f2b3a611e7ba3c3f218c777
FRanggaY <franciscusrangga@gmail.com>
2024-04-25 15:40:58 +0700

    update : add navbar

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -14,16 +14,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { ThemeToggle } from '@/components/theme-toggle'
 import { useTranslations } from 'next-intl';
 import { LanguageToggle } from '@/components/languge-toggle'
+import { NavigationBar } from '@/components/navigation-bar'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
-  description: 'Example dashboard app built using the components.'
+  description: ''
 }
 
 export default function DashboardPage() {
   const t = useTranslations('Index');
   return (
     <>
+      <NavigationBar />
       <div className='md:hidden'>
         <Image
           src='/images/dashboard-light.png'
@@ -40,7 +42,7 @@ export default function DashboardPage() {
           className='hidden dark:block'
         />
       </div>
-      <div className='hidden flex-col md:flex'>
+      <div className='flex-col md:flex'>
         <div className='border-b'>
           <div className='flex h-16 items-center px-4'>
             {/* <TeamSwitcher />
@@ -56,10 +58,6 @@ export default function DashboardPage() {
         <div className='flex-1 space-y-4 p-8 pt-6'>
           <div className='flex items-center justify-between space-y-2'>
             <h2 className='text-3xl font-bold tracking-tight'>Dashboard</h2>
-            <div className='flex items-center space-x-2'>
-              {/* <CalendarDateRangePicker /> */}
-              <Button>Download</Button>
-            </div>
           </div>
           <Tabs defaultValue='overview' className='space-y-4'>
             <TabsList>
@@ -175,27 +173,6 @@ export default function DashboardPage() {
                     <p className='text-xs text-muted-foreground'>
                       +201 since last hour
                     </p>
-                  </CardContent>
-                </Card>
-              </div>
-              <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-7'>
-                <Card className='col-span-4'>
-                  <CardHeader>
-                    <CardTitle>Overview</CardTitle>
-                  </CardHeader>
-                  <CardContent className='pl-2'>
-                    {/* <Overview /> */}
-                  </CardContent>
-                </Card>
-                <Card className='col-span-3'>
-                  <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
-                    <CardDescription>
-                      You made 265 sales this month.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    {/* <RecentSales /> */}
                   </CardContent>
                 </Card>
               </div>
